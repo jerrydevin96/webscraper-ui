141f518c90811caf09de79c9c89be4fa6a86b4bf
Jerry Devin <jerrydevin96@gmail.com>
2021-05-29 14:01:51 +0530

    basic application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private httpClient: HttpClient) {
+  }
   title = 'webscraper-ui';
+  resultCardData : any = {
+    pageTitle: "",
+    htmlVersion: "",
+    h1Length: 0,
+    h2Length: 0,
+    h3Length: 0,
+    h4Length: 0,
+    h5Length: 0,
+    h6Length: 0,
+    internalLinksLength: 0,
+    externalLinksLength: 0,
+    inAccessibleLinksLength: 0
+  }
+  testUrl = "";
+  displayDataCard = false
+  displaySpinnerCard = false
+
+  getPageData(url: string): Observable<any> {
+    var requestData = {
+      "url": url
+    }
+    console.log(requestData)
+    console.log(JSON.stringify(requestData))
+    return this.httpClient.post("http://ec2-174-129-50-191.compute-1.amazonaws.com:8080/v0/PageDetails", JSON.stringify(requestData))
+  }
+
+  readURLValue(event : any) {
+    this.testUrl = event.target.value
+  }
+
+  analyzeURL() {
+    console.log(this.testUrl)
+    this.displayDataCard = false
+    this.displaySpinnerCard = true
+    this.getPageData(this.testUrl).subscribe( res => {
+      console.log(res)
+      this.displayDataCard = true
+      this.displaySpinnerCard = false
+      this.resultCardData.pageTitle = res.pageTitle
+      this.resultCardData.htmlVersion = res.htmlVersion
+      this.resultCardData.h1Length = res.h1Length
+      this.resultCardData.h2Length = res.h2Length
+      this.resultCardData.h3Length = res.h3Length
+      this.resultCardData.h4Length = res.h4Length
+      this.resultCardData.h5Length = res.h5Length
+      this.resultCardData.h6Length = res.h6Length
+      this.resultCardData.internalLinksLength = res.internalLinks
+      this.resultCardData.externalLinksLength = res.externalLinks
+      this.resultCardData.inAccessibleLinksLength = res.inaccessibleLinks
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
